4a6b308668728dff4e69cf87996f7525cb75178a
efekos <hefeolgun10@gmail.com>
2023-09-10 20:50:55 +0300

    fix configuration being null by default

--- src/main/java/me/efekos/simpler/Simpler.java
+++ src/main/java/me/efekos/simpler/Simpler.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Main class of Simpler. does not have a use right now.
  */
 public final class Simpler extends JavaPlugin {
-    private static MessageConfiguration configuration;
+    private static MessageConfiguration configuration = new MessageConfiguration.Builder().build();
 
     public static MessageConfiguration getMessageConfiguration() {
         return configuration;
@@ -19,7 +19,6 @@ public final class Simpler extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        configuration = new MessageConfiguration.Builder().build();
         Metrics metrics = new Metrics(this,19686);
     }
 }
