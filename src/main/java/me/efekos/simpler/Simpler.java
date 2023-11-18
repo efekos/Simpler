72151f2680ddeb289b9e43e58265e23f81e3f545
efekos <hefeolgun10@gmail.com>
2023-11-18 11:52:53 +0300

    add JavaDoc, remove Metrics and add #hasPlaceholderApi

--- src/main/java/me/efekos/simpler/Simpler.java
+++ src/main/java/me/efekos/simpler/Simpler.java
@@ -1,24 +1,50 @@
 package me.efekos.simpler;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.PlaceholderAPIPlugin;
 import me.efekos.simpler.config.MessageConfiguration;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Main class of Simpler. does not have a use right now.
+ * Main class of Simpler.
  */
 public final class Simpler extends JavaPlugin {
+    /**
+     * Main configuration for messages that Simpler uses.
+     */
     private static MessageConfiguration configuration = new MessageConfiguration.Builder().build();
 
+    /**
+     * Returns the message configuration Simpler is using right now.
+     * @return Message config of Simpler.
+     */
     public static MessageConfiguration getMessageConfiguration() {
         return configuration;
     }
 
+    /**
+     * Changes the configuration with the configuration given.
+     * @param configuration New configuration that you want Simpler to use.
+     */
     public static void changeMessageConfiguration(MessageConfiguration configuration) {
         Simpler.configuration = configuration;
     }
 
+    /**
+     * Checks for PlaceholderAPI existence in server.
+     * @return whether the server has PlaceholderAPI
+     */
+    public static boolean hasPlaceholderApi(){
+        try {
+            return Bukkit.getServer().getPluginManager().isPluginEnabled(PlaceholderAPIPlugin.getInstance());
+        } catch (Exception e){
+            return Bukkit.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI");
+        }
+    }
+
     @Override
     public void onEnable() {
-        Metrics metrics = new Metrics(this,19686);
+
     }
 }
