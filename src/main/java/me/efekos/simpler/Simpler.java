7ae6987aa5e08e15b06aaadc33ff25e9f7c5e08b
efekos <hefeolgun10@gmail.com>
2023-11-26 22:30:06 +0300

    add test command

--- src/main/java/me/efekos/simpler/Simpler.java
+++ src/main/java/me/efekos/simpler/Simpler.java
@@ -1,5 +1,9 @@
 package me.efekos.simpler;
 
+import me.efekos.simpler.commands.CommandManager;
+import me.efekos.simpler.commands.CommandTree;
+import me.efekos.simpler.commands.node.impl.LabelNode;
+import me.efekos.simpler.commands.node.impl.StringArgumentNode;
 import me.efekos.simpler.config.MessageConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -30,6 +34,16 @@ public final class Simpler extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        try {
+            CommandManager.registerCommandTree(this,new CommandTree("test","test command","test.test")
+                    .addChild(new LabelNode("hello")
+                            .addChild(new StringArgumentNode().setExecutive(context -> context.getSender().sendMessage(context.getArgs().toString())))
+                    )
+            );
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
         //this thing simply makes a /home set <pos> command with nms. I'll use it later 👀👀
         //CommandDispatcher<CommandListenerWrapper> dispatcher = MinecraftServer.getServer().vanillaCommandDispatcher.a();
