24588d74c94bb076c062a41eeab54f0450e96cd8
efekos <hefeolgun10@gmail.com>
2024-02-06 08:11:28 +0300

    change method finding system

--- src/main/java/me/efekos/simpler/items/AbstractCustomItem.java
+++ src/main/java/me/efekos/simpler/items/AbstractCustomItem.java
@@ -29,19 +29,16 @@ import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
 abstract class AbstractCustomItem {
     private final Consumer<ItemStack> appearance;
-    private final List<Method> methods = new ArrayList<>();
-    private final Map<SaveField,Field> saveFieldsMap = new HashMap<>();
+    private final Map<HandleEvent, Method> methodMap = new HashMap<>();
+    private final Map<SaveField, Field> saveFieldsMap = new HashMap<>();
 
     AbstractCustomItem(Consumer<ItemStack> appearance) {
         this.appearance = appearance;
@@ -52,8 +49,8 @@ abstract class AbstractCustomItem {
     private void findSaveFields() {
         for (Field field : getClass().getDeclaredFields()) {
             SaveField annotation = field.getAnnotation(SaveField.class);
-            if(annotation==null)continue;
-            saveFieldsMap.put(annotation,field);
+            if (annotation == null) continue;
+            saveFieldsMap.put(annotation, field);
         }
     }
 
@@ -66,81 +63,80 @@ abstract class AbstractCustomItem {
     }
 
     void runMethods(Event event) {
-        for (Method method : methods) {
-            try {
-                method.invoke(this, event);
-            } catch (InvocationTargetException ignored) {
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        methodMap.forEach((handleEvent, method) -> {
+            //TODO
+        });
     }
 
-    void putSaveFields(JsonObject object){
-        if(saveFieldsMap.isEmpty()) findSaveFields();
+    void putSaveFields(JsonObject object) {
+        if (saveFieldsMap.isEmpty()) findSaveFields();
         saveFieldsMap.forEach((annotation, field) -> {
             String s = annotation.value();
 
             try {
                 boolean b = Modifier.isPrivate(field.getModifiers());
-                if(b) field.setAccessible(true);
+                if (b) field.setAccessible(true);
                 Object o = field.get(this);
 
-                switch (annotation.fieldType()){
-                    case STRING -> object.addProperty(s,o.toString());
-                    case FLOAT -> object.addProperty(s,o instanceof Float?(Float)o:(float)o);
-                    case LONG -> object.addProperty(s,o instanceof Long?(Long)o:(long)o);
-                    case DOUBLE -> object.addProperty(s,o instanceof Double?(Double)o:(double)o);
-                    case INTEGER -> object.addProperty(s,o instanceof Integer?(Integer)o:(int)o);
-                    case BYTE -> object.addProperty(s,o instanceof Byte?(Byte)o:(byte)o);
-                    case BOOLEAN -> object.addProperty(s,o instanceof Boolean?(Boolean) o:(boolean)o);
+                switch (annotation.fieldType()) {
+                    case STRING -> object.addProperty(s, o.toString());
+                    case FLOAT -> object.addProperty(s, o instanceof Float ? (Float) o : (float) o);
+                    case LONG -> object.addProperty(s, o instanceof Long ? (Long) o : (long) o);
+                    case DOUBLE -> object.addProperty(s, o instanceof Double ? (Double) o : (double) o);
+                    case INTEGER -> object.addProperty(s, o instanceof Integer ? (Integer) o : (int) o);
+                    case BYTE -> object.addProperty(s, o instanceof Byte ? (Byte) o : (byte) o);
+                    case BOOLEAN -> object.addProperty(s, o instanceof Boolean ? (Boolean) o : (boolean) o);
                 }
-                if(b) field.setAccessible(false);
-            } catch (Exception e){
+                if (b) field.setAccessible(false);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         });
 
     }
 
-    void loadSaveFields(JsonObject object){
-        if(saveFieldsMap.isEmpty()) findSaveFields();
+    void loadSaveFields(JsonObject object) {
+        if (saveFieldsMap.isEmpty()) findSaveFields();
 
         saveFieldsMap.forEach((key, field) -> {
             JsonElement jsonElement = object.get(key.value());
 
             boolean b = Modifier.isPrivate(field.getModifiers());
-            if(b) field.setAccessible(true);
+            if (b) field.setAccessible(true);
             try {
-                switch (key.fieldType()){
-                    case STRING -> field.set(this,jsonElement.getAsString());
-                    case INTEGER -> field.set(this,jsonElement.getAsInt());
-                    case DOUBLE -> field.set(this,jsonElement.getAsDouble());
-                    case LONG -> field.set(this,jsonElement.getAsLong());
-                    case BOOLEAN -> field.set(this,jsonElement.getAsBoolean());
-                    case FLOAT -> field.set(this,jsonElement.getAsFloat());
-                    case BYTE -> field.set(this,jsonElement.getAsByte());
+                switch (key.fieldType()) {
+                    case STRING -> field.set(this, jsonElement.getAsString());
+                    case INTEGER -> field.set(this, jsonElement.getAsInt());
+                    case DOUBLE -> field.set(this, jsonElement.getAsDouble());
+                    case LONG -> field.set(this, jsonElement.getAsLong());
+                    case BOOLEAN -> field.set(this, jsonElement.getAsBoolean());
+                    case FLOAT -> field.set(this, jsonElement.getAsFloat());
+                    case BYTE -> field.set(this, jsonElement.getAsByte());
                 }
-            } catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             } finally {
-                if(b) field.setAccessible(false);
+                if (b) field.setAccessible(false);
             }
 
         });
     }
 
     void findMethods() {
-        Method[] allMethods = this.getClass().getMethods();
+        Method[] allMethods = getClass().getDeclaredMethods();
 
         for (Method method : allMethods) {
-            if (method.getAnnotation(HandleEvent.class) != null) {
+            boolean b = Modifier.isPrivate(method.getModifiers());
+            if (b) method.setAccessible(true);
 
-                if (!Modifier.isPublic(method.getModifiers()))
-                    throw new RuntimeException(new InvalidAnnotationException("me.efekos.simpler.items.HandleEvent must be applied to a public method, " + method.getName() + " is not."));
+            HandleEvent annotation = method.getAnnotation(HandleEvent.class);
+            if (annotation == null) continue;
 
-                methods.add(method);
-            }
+            if (!Modifier.isPublic(method.getModifiers()))
+                throw new RuntimeException(new InvalidAnnotationException("me.efekos.simpler.items.HandleEvent must be applied to a public method, " + method.getName() + " is not."));
+            else methodMap.put(annotation,method);
+
+            if (b) method.setAccessible(false);
         }
     }
 }
